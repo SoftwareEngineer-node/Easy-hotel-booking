bf1437ffa8a8378191bea9baef1d22d978cfcba4
SoftwareEngineer-node <SoftwareEngineer-node@gmail.com>
2024-09-25 22:27:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(json());
 app.use(cors());
 app.use(helmet());
